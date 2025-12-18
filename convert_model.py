88f5fcbd3b99561c4ae5586284cfa6a8cdeb6f61
th-pr-mg <112316003+hmwh4t@users.noreply.github.com>
2025-12-18 13:45:14 +0700

    Removed PhoWhisper implementation

--- convert_model.py
+++
@@ -1,6 +0,0 @@
-from ctranslate2.converters import TransformersConverter
-
-print("Converting model...")
-converter = TransformersConverter("vinai/PhoWhisper-medium")
-converter.convert("PhoWhisper-medium-ct2", quantization="float16")
-print("Conversion complete.")
